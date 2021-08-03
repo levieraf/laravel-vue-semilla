8b5e6f162a1a1f95525a326e5635350e77451dbd
Luis Viera <levieraf@gmail.com>
2021-08-03 11:58:54 -0500

    primer commit

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+require('./bootstrap');
+import * as Vue from 'vue';
+import App from './components/App/App.vue';
+
+Vue.createApp(App).mount('#app');
---
+++ resources/js/components/App/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="titulo">Ejemplo</div>
+    <button @click="count++">
+        You clicked me {{ count }} times.
+    </button>
+  </div>
+</template>
+
+<script>
+const App =  {
+  name: "app",
+  data() {
+    return {
+      count: 0
+    }
+  },
+}
+
+export default App;
+</script>
+
+<style>
+.titulo {
+  padding-bottom: 15px;
+}
+</style>
